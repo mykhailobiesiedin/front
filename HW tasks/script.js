dffbc145e4377d92effce688e8bf8e485f1d1985
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-04-09 13:43:24 +0300

    Add files via upload

---
+++ HW tasks/script.js
@@ -0,0 +1,85 @@
+// *** Task 1 ***
+// let d = 7;
+// console.log(d);
+// console.log(Math.pow(d,2));
+// console.log(Math.pow(d,3));
+
+// *** Task 2 ***
+// let num = Number(prompt('Enter the number : '));
+// if(num > 0){
+//     console.log(num*2);
+// } 
+// else if(num ===0){
+//     console.log(0);
+// }
+// else{
+//     console.log('Your number is negative')
+// }
+
+// *** Task 3 ***
+// let num1 = Number(prompt('Enter the 1 number : '));
+// let num2 = Number(prompt('Enter the 2 number : '));
+// if(num1 > num2){
+//     console.log(num1 + num2);
+// }
+// else if(num1 === num2){
+//     console.log('Your numbers are equal');
+// } else {
+//     console.log(num1*num2);
+// }
+
+// *** Task 4 ***
+// let num = Number(prompt('Enter the number : '));
+// if(num <0){
+//     console.log(Math.pow(num, 2));
+// } 
+// else{
+//     console.log("Error");
+// }
+
+// *** Task 5 ***
+// let num_of_pupils = Number(prompt('Enter the number of pupils : '));
+// let num_of_chairs = Number(prompt('Enter the number of chairs : '));
+// if(num_of_pupils <= num_of_chairs){
+//     console.log('Pupils : ' + num_of_pupils + ', ' + 'chairs : ' + num_of_chairs + ' There are enough chairs')
+// }
+// else{
+//     console.log('Pupils : ' + num_of_pupils + ', ' + 'chairs : ' + num_of_chairs + ' There are not enough chairs')
+// }
+
+// *** Task 6 ***
+//     console.log('Start');
+// for(let i = 0; i<10; i++){
+//     console.log(i);
+// }
+//     console.log('Finish');
+
+// *** Task 7 ***
+// for(let i = 10; i<100; i++){
+//     if(i%5 ==0){
+//         console.log(i);
+//     }
+// }
+
+// *** Task 8 ***
+// let arr = [18, 12, 26, 144, 0, -7, 190];
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i] >= 15){
+//         console.log(arr[i])
+//     }
+// }
+
+// *** Task 9 ***
+// for(let i = 0; i<10; i++){
+//     if(i%2 > 0){
+//         console.log(i);
+//     }
+// }
+
+// *** Task 10 ***
+// let arr = [18, 12, 26, 144,-7, 66, 13, 15];
+// for(let i = 0; i<arr.length; i++){
+//     if(i%3 == 0){
+//         console.log(arr[i]);
+//     }
+// }
